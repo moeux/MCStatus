adf69d0ea5ed9903c3820ede754f09a05284b5f4
moeux <68925026+moeux@users.noreply.github.com>
2025-03-21 16:23:53 +0100

    Fixed naming and added cancellation support.

--- MCStatus/Utilities/Pinger.cs
+++ MCStatus/Utilities/Pinger.cs
@@ -6,25 +6,32 @@ namespace MCStatus.Utilities;
 
 public static class Pinger
 {
-    public static async Task<Status?> RequestStatus(Server server)
+    public static async Task<Status?> RequestStatusAsync(Server server, CancellationToken cancellationToken = default)
     {
         using var client = new TcpClient();
-        await client.ConnectAsync(server.Address, server.Port);
+        await client.ConnectAsync(server.Address, server.Port, cancellationToken);
         await using var stream = client.GetStream();
 
-        await SendHandshake(stream, server.ProtocolVersion, server.Address, server.Port);
-        await SendStatusRequest(stream);
+        await SendHandshakeAsync(stream, server.ProtocolVersion, server.Address, server.Port, cancellationToken);
+        await SendStatusRequestAsync(stream, cancellationToken);
         var status = ParseStatusResponse(stream);
 
         if (status is null) return null;
 
-        await SendPingRequest(stream);
-        status.Ping = ParsePingResponse(stream);
+        await SendPingRequestAsync(stream, cancellationToken);
+        var start = DateTimeOffset.FromUnixTimeMilliseconds(ParsePingResponse(stream));
+        var latency = DateTimeOffset.UtcNow.Subtract(start);
+        status.Ping = latency.TotalMilliseconds;
 
         return status;
     }
 
-    private static async Task SendHandshake(NetworkStream stream, int protocolVersion, string address, ushort port)
+    private static async Task SendHandshakeAsync(
+        NetworkStream stream,
+        int protocolVersion,
+        string address,
+        ushort port,
+        CancellationToken cancellationToken = default)
     {
         var bytes = new List<byte>();
 
@@ -38,10 +45,11 @@ public static class Pinger
         // Prefix with total packet length
         bytes.InsertRange(0, DataCodec.EncodeVarInt(bytes.Count));
 
-        await stream.WriteAsync(bytes.ToArray());
+        await stream.WriteAsync(bytes.ToArray(), cancellationToken);
     }
 
-    private static async Task SendStatusRequest(NetworkStream stream)
+    private static async Task SendStatusRequestAsync(NetworkStream stream,
+        CancellationToken cancellationToken = default)
     {
         var bytes = new List<byte>();
         // Packet ID 0 for status request
@@ -49,10 +57,10 @@ public static class Pinger
         // Prefix with total packet length
         bytes.InsertRange(0, DataCodec.EncodeVarInt(bytes.Count));
 
-        await stream.WriteAsync(bytes.ToArray());
+        await stream.WriteAsync(bytes.ToArray(), cancellationToken);
     }
 
-    private static async Task SendPingRequest(NetworkStream stream)
+    private static async Task SendPingRequestAsync(NetworkStream stream, CancellationToken cancellationToken = default)
     {
         var bytes = new List<byte>();
 
@@ -63,7 +71,7 @@ public static class Pinger
         // Prefix with total packet length
         bytes.InsertRange(0, DataCodec.EncodeVarInt(bytes.Count));
 
-        await stream.WriteAsync(bytes.ToArray());
+        await stream.WriteAsync(bytes.ToArray(), cancellationToken);
     }
 
     private static Status? ParseStatusResponse(NetworkStream stream)
