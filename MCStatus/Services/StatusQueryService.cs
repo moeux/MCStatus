235486e93acf05b498ce10cd9f11b68a064152c2
moeux <68925026+moeux@users.noreply.github.com>
2025-03-22 03:35:15 +0100

    Fixed caching.

--- MCStatus/Services/StatusQueryService.cs
+++ MCStatus/Services/StatusQueryService.cs
@@ -8,17 +8,13 @@ public class StatusQueryService(IMemoryCache cache)
 {
     public async Task<Status?> RequestStatusAsync(Server server, CancellationToken cancellationToken = default)
     {
-        if (!cache.TryGetValue(server, out Status? status) || status is null)
-        {
-            status = await Pinger.RequestStatusAsync(server, cancellationToken);
+        if (cache.TryGetValue(server, out Status? status) && status is not null)
+            return status;
 
-            if (status is not null)
-            {
-                var cacheEntry = cache.CreateEntry(server);
-                cacheEntry.SetValue(status);
-                cacheEntry.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
-            }
-        }
+        status = await Pinger.RequestStatusAsync(server, cancellationToken);
+
+        if (status is not null)
+            cache.Set(server, status, TimeSpan.FromMinutes(1));
 
         return status;
     }
