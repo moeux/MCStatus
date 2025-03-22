1949ae67c3b120c8b421ba906559437cfd012f31
moeux <68925026+moeux@users.noreply.github.com>
2025-03-22 03:16:17 +0100

    Added exception response, refactoring and fixed Base64 and input converting.

--- MCStatus/Commands/StatusCommand.cs
+++ MCStatus/Commands/StatusCommand.cs
@@ -17,71 +17,130 @@ public class StatusCommand(StatusQueryService service) : ICommandHandler
     {
         var ipOrDomain = command.Data.Options
             .Where(option => option.Name is "server")
-            .Select(option => option.Value)
-            .Cast<string>()
+            .Select(option => Convert.ToString(option.Value))
             .First();
         var port = command.Data.Options
             .Where(option => option.Name is "port")
-            .Select(option => option.Value)
-            .Cast<ushort>()
+            .Select(option => Convert.ToUInt16(option.Value))
             .FirstOrDefault((ushort)25565);
 
         if (string.IsNullOrWhiteSpace(ipOrDomain))
         {
-            if (command.UserLocale is "de-DE")
-                await command.RespondAsync("Bitte gib eine gültige IP oder Domain an!",
-                    options: GetRequestOptions(cancellationToken));
-            else
-                await command.RespondAsync("Please specify a valid IP or domain!",
-                    options: GetRequestOptions(cancellationToken));
+            await Respond(
+                command,
+                "Please specify a valid IP or domain!",
+                "Bitte gib eine gültige IP oder Domain an!",
+                cancellationToken
+            );
             return;
         }
 
         if (port < 1024)
         {
-            if (command.UserLocale is "de-DE")
-                await command.RespondAsync("Nur Ports größer 1023 sind erlaubt!",
-                    options: GetRequestOptions(cancellationToken));
-            else
-                await command.RespondAsync("Only ports greater than 1023 are allowed!",
-                    options: GetRequestOptions(cancellationToken));
+            await Respond(
+                command,
+                "Only ports greater than 1023 are allowed!",
+                "Nur Ports größer 1023 sind erlaubt!",
+                cancellationToken
+            );
             return;
         }
 
-        var status = await service.RequestStatusAsync(new Server
+        try
         {
-            Address = ipOrDomain,
-            Port = port,
-            ProtocolVersion = 769
-        }, cancellationToken);
+            var status = await service.RequestStatusAsync(new Server
+            {
+                Address = ipOrDomain,
+                Port = port,
+                ProtocolVersion = 769
+            }, cancellationToken);
 
-        if (status is null)
-        {
-            if (command.UserLocale is "de-DE")
-                await command.RespondAsync("Der Server ist leider nicht erreichbar.",
-                    options: GetRequestOptions(cancellationToken));
-            else
-                await command.RespondAsync("The server is unfortunately not reachable.",
-                    options: GetRequestOptions(cancellationToken));
-            return;
+            if (status is null)
+            {
+                await Respond(
+                    command,
+                    "The server is unfortunately not reachable.",
+                    "Der Server ist leider nicht erreichbar.",
+                    cancellationToken
+                );
+                return;
+            }
+
+            // The FavIcon string is prefixed with data:image/png;base64,<data>, so we create a substring first
+            var bytes = Convert.FromBase64String(status.FavIcon[(status.FavIcon.LastIndexOf(',') + 1)..]);
+            using var stream = new MemoryStream(bytes);
+            using var fileAttachment = new FileAttachment(stream, $"{Guid.NewGuid()}.png", isThumbnail: true);
+
+            await command.RespondWithFileAsync(
+                fileAttachment,
+                embed: CreateEmbed(ipOrDomain, port, fileAttachment.FileName, status),
+                options: GetRequestOptions(cancellationToken)
+            );
         }
+        catch (Exception)
+        {
+            await Respond(
+                command,
+                "Unfortunately this did not work, please check your input!",
+                "Das hat leider nicht funktioniert, bitte überprüfe deine Angaben!",
+                cancellationToken
+            );
+        }
+    }
 
+    private static RequestOptions GetRequestOptions(CancellationToken cancellationToken)
+    {
+        return new RequestOptions { CancelToken = cancellationToken };
+    }
+
+    private static Color GetGradientColor(double ping)
+    {
+        var ratio = Math.Max(0, Math.Min(120, ping)) / 120.0;
+        var red = (int)(ratio * 255);
+        var green = (int)((1 - ratio) * 255);
+
+        return new Color(red, green, 0);
+    }
+
+    private static async Task Respond(
+        SocketSlashCommand command,
+        string defaultText,
+        string localizedText,
+        CancellationToken cancellationToken = default)
+    {
+        if (command.UserLocale is "de")
+            await command.RespondAsync(
+                localizedText,
+                ephemeral: true,
+                options: GetRequestOptions(cancellationToken)
+            );
+        else
+            await command.RespondAsync(
+                defaultText,
+                ephemeral: true,
+                options: GetRequestOptions(cancellationToken)
+            );
+    }
+
+    private static Embed CreateEmbed(string ipOrDomain, ushort port, string fileName, Status status)
+    {
         var embed = new EmbedBuilder();
-        var fileName = $"{Guid.NewGuid()}.png";
-        var bytes = Convert.FromBase64String(status.FavIcon);
-        using var stream = new MemoryStream(bytes);
-        using var fileAttachment = new FileAttachment(stream, fileName, isThumbnail: true);
 
-        embed.WithColor(GetGradientColor(status.Ping));
-        embed.WithCurrentTimestamp();
-        embed.WithThumbnailUrl($"attachment://{fileName}");
-        embed.AddField("Description", status.Description.Text);
-        embed.AddField("Online Players", status.Players.Online);
-        embed.AddField("Max Players", status.Players.Max, true);
-        embed.AddField("Version", status.Version.ToString(), true);
-        embed.AddField("Ping", $"{status.Ping:F2}", true);
-        embed.AddField("Enforces Secure Chat?", status.EnforcesSecureChat ? "\u2705" : "\u274c");
-        embed.AddField("Prevents Chat Reports?", status.PreventsChatReports ? "\u2705" : "\u274c", true);
+        embed.WithTitle($"{ipOrDomain}:{port}")
+            .WithColor(GetGradientColor(status.Ping))
+            .WithCurrentTimestamp()
+            .WithThumbnailUrl($"attachment://{fileName}")
+            .AddField("Description", status.Description.Text)
+            .AddField("Online Players", status.Players.Online, true)
+            .AddField("Max Players", status.Players.Max, true)
+            // Empty field for layout purposes
+            .AddField("\u200B", "\u200B", true)
+            .AddField("Version", status.Version.ToString(), true)
+            .AddField("Ping", $"{status.Ping:F2} ms", true)
+            // Empty field for layout purposes
+            .AddField("\u200B", "\u200B", true)
+            .AddField("Enforces Secure Chat?", status.EnforcesSecureChat ? "\u2705" : "\u274c", true)
+            .AddField("Prevents Chat Reports?", status.PreventsChatReports ? "\u2705" : "\u274c", true);
 
         if (status.ModInfo is not null)
         {
@@ -89,21 +148,6 @@ public class StatusCommand(StatusQueryService service) : ICommandHandler
             embed.AddField("Mods Count", status.ModInfo.ModList.Count(), true);
         }
 
-        await command.RespondWithFileAsync(
-            fileAttachment, embed: embed.Build(), options: GetRequestOptions(cancellationToken));
-    }
-
-    private static Color GetGradientColor(double ping)
-    {
-        var ratio = Math.Max(0, Math.Min(200, ping)) / 200.0;
-        var red = (int)(ratio * 255);
-        var green = (int)((1 - ratio) * 255);
-
-        return new Color(red, green, 0);
-    }
-
-    private static RequestOptions GetRequestOptions(CancellationToken cancellationToken)
-    {
-        return new RequestOptions { CancelToken = cancellationToken };
+        return embed.Build();
     }
 }
